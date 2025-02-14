6a5e5267bc3486fbbbc5e66c4f7abf410d1393b6
Ksl022205 <c27kyle.litman@afacademy.af.edu>
2025-02-13 21:48:39 -0700

    Update top_basys3.vhd

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -67,21 +67,21 @@ architecture top_basys3_arch of top_basys3 is
     end component;
 
     -- declare any signals you will need	
-    signal sum   : std_logic_vector(3 downto 0);
-    signal carry : std_logic;
+    signal w_sw   : std_logic_vector(15 downto 0);
+    signal w_led : std_logic_vector (15 downto 0);
 begin
 	-- PORT MAPS --------------------
 	ripple_adder_inst : ripple_adder
     port map (
-            A    => sum(4 downto 1),
-            B    => sum(15 downto 12),
-            Cin  => sum(0),
+            A    => w_sw(4 downto 1),
+            B    => w_sw(15 downto 12),
+            Cin  => w_sw(0),
             S    => led(3 downto 0),
             Cout => led(15)
         );
 	---------------------------------
 	-- CONCURRENT STATEMENTS --------
 	led(14 downto 4) <= (others => '0'); -- Ground unused LEDs
-	sum <= sw;
+	w_sw <= sw;
 	---------------------------------
 end top_basys3_arch;
