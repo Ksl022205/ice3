25e0e81e2404be558b396769bfbbbedffadcef54
Ksl022205 <c27kyle.litman@afacademy.af.edu>
2025-02-13 21:46:11 -0700

    Update top_basys3.vhd

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -56,15 +56,32 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
     -- declare the component of your top-level design
+ component ripple_adder
+        port (
+            A    : in  std_logic_vector(3 downto 0);
+            B    : in  std_logic_vector(3 downto 0);
+            Cin  : in  std_logic;
+            S    : out std_logic_vector(3 downto 0);
+            Cout : out std_logic
+        );
+    end component;
 
     -- declare any signals you will need	
-  
+    signal sum   : std_logic_vector(3 downto 0);
+    signal carry : std_logic;
 begin
 	-- PORT MAPS --------------------
-   
+	ripple_adder_inst : ripple_adder
+    port map (
+            A    => sum(4 downto 1),
+            B    => sum(15 downto 12),
+            Cin  => sum(0),
+            S    => led(3 downto 0),
+            Cout => led(15)
+        );
 	---------------------------------
-	
 	-- CONCURRENT STATEMENTS --------
 	led(14 downto 4) <= (others => '0'); -- Ground unused LEDs
+	sum <= sw;
 	---------------------------------
 end top_basys3_arch;
